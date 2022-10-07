9e3a9a7c2e63f281ede8f3231c15c6bbd5a888d1
anilpdv <pdvanil007@gmail.com>
2022-10-07 23:15:28 +0530

    Initial Commit

---
+++ QuotesApp/QuotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesAppApp.swift
+//  QuotesApp
+//
+//  Created by anilpdv on 07/10/22.
+//
+
+import SwiftUI
+
+@main
+struct QuotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
